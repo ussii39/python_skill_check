2d5a6893b488d2965ac9d4a00fe8e4e66bd5a134
ussii39 <usimarutetunori3@outlook.jp>
2025-02-16 19:50:02 +0900

    回文判定機能を追加し、重複削除アルゴリズムを整理

--- leetCode.py
+++ leetCode.py
@@ -1,42 +1,44 @@
-from typing import List, Union
-from itertools import groupby
+# すべての大文字を小文字に変換し、英数字以外の文字をすべて削除した後、前後どちらで読んでも同じになるフレーズは回文です。英数字には、文字と数字が含まれます。
+
+# 文字列が与えられた場合s、trueそれが回文であれば を返し、falseそうでない場合は を返します。
+
+
+# 例1:
+
+# 入力: s = "A man, a plan, a canal: Panama"
+# 出力: true
+# 説明: "amanaplanacanalpanama" は回文です。
+# 例2:
+
+# 入力: s = "race a car"
+# 出力: false
+# 説明: "raceacar" は回文ではありません。
+# 例3:
+
+# 入力: s = " "
+# 出力: true
+# 説明: s は、英数字以外の文字を削除した後の空の文字列 "" です。
+# 空の文字列は前後どちらから読んでも同じなので、回文になります。
+
+
+# 制約:
+
+# 1 <= s.length <= 2 * 105
+# s印刷可能な ASCII 文字のみで構成されます。
+
+import re
 
 
 class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        # 配列が2以下なら変更の必要なし
-        if len(nums) <= 2:
-            return len(nums)
+    def isPalindrome(self, s: str) -> bool:
+        string = "".join(s.split())
+        pattern = f"[^a-zA-Z0-9]"
+        text = re.sub(pattern, "", string).lower()
 
-        # insertする位置を示すポインタ
-        k = 2
-
-        # 3番目の要素から順にチェック
-        for i in range(2, len(nums)):
-            # 現在の数が2つ前の位置の数と異なれば追加可能
-            if nums[i] != nums[k - 2]:
-                nums[k] = nums[i]
-                k += 1
-
-        return k
+        reversed_text = "".join(reversed(text))
+        return text == reversed_text
 
 
 s = Solution()
-
-nums: list[int] = [1, 1, 1, 2, 2, 3]
-
-expectedNums: list[Union[int, str]] = [
-    1,
-    1,
-    2,
-    2,
-    3,
-]
-
-
-k: int = s.removeDuplicates(nums)
-
-assert k == len(expectedNums)
-
-for i in range(k):
-    assert nums[i] == expectedNums[i]
+expected_output = True
+assert expected_output == s.isPalindrome(s="A man, a plan, a canal: Panama")
