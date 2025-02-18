9cb44a8beb672f593565fc12750ecb564d71288e
ussii39 <usimarutetunori3@outlook.jp>
2025-02-18 13:38:36 +0900

    同型文字列判定機能を追加し、ランサムノート問題のコードを削除

--- leetCode.py
+++ leetCode.py
@@ -1,11 +1,30 @@
-# https://leetcode.com/problems/ransom-note/?envType=study-plan-v2&envId=top-interview-150
-from collections import Counter
+# https://leetcode.com/problems/isomorphic-strings/?envType=study-plan-v2&envId=top-interview-150
+from collections import defaultdict
 
 
 class Solution:
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        return not Counter(ransomNote) - Counter(magazine)
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        s_d = defaultdict(str)
+        t_d = defaultdict(str)
+        t_len = len(t)
+        s_len = len(s)
+        if t_len != s_len:
+            return False
+        for i in range(t_len):
+            if not s_d[s[i]] and not t_d[t[i]]:
+                s_d[s[i]] = t[i]
+                t_d[t[i]] = s[i]
+
+            elif s_d[s[i]] != t[i] or t_d[t[i]] != s[i]:
+                return False
+        return True
 
 
+test_cases = [
+    {"s": "egg", "t": "add", "expected_value": True},
+    {"s": "foo", "t": "bar", "expected_value": False},
+    {"s": "paper", "t": "title", "expected_value": True},
+]
 s = Solution()
-print(s.canConstruct(ransomNote="a", magazine="b"))
+for case in test_cases:
+    print(s.isIsomorphic(case["s"], case["t"]))
