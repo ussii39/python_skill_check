bc536df081bfecfdf80d4f64062aed1753724d00
ussii39 <usimarutetunori3@outlook.jp>
2025-02-24 13:25:27 +0900

    最大平均部分配列を計算する関数を追加

--- leetCode.py
+++ leetCode.py
@@ -1,17 +1,21 @@
-from typing import Set
+# https://leetcode.com/problems/maximum-average-subarray-i/?envType=study-plan-v2&envId=leetcode-75
+from typing import List
 
 
 class Solution:
-    def findDifference(self, nums1: list[int], nums2: list[int]) -> list[list[int]]:
-        # n1_len = len(nums1)
-        # n2_len = len(nums2)
-        set1 = set(nums1)
-        set2 = set(nums2)
-        unique_to_num1 = list(set1 - set2)
-        unique_to_num2 = list(set2 - set1)
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        # 最初のウィンドウの平均値を初期値とする
+        max_number = sum(nums[:k]) / k
 
-        return [unique_to_num1, unique_to_num2]
+        # 残りのウィンドウをスライドして計算
+        for i in range(len(nums) - k):
+            curr_avg = sum(nums[i + 1 : i + k + 1]) / k
+            max_number = max(max_number, curr_avg)
+
+        return max_number
 
 
 s = Solution()
-print(s.findDifference(nums1=[1, 2, 3], nums2=[2, 4, 6]))
+print(s.findMaxAverage(nums=[5], k=1))  # 5
+print(s.findMaxAverage(nums=[-1], k=1))  # -1
+print(s.findMaxAverage(nums=[1, 12, -5, -6, 50, 3], k=4))  # 12.75
