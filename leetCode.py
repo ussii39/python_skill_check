026b7d62baf23b6f8c1e029578dcc406282d99cd
ussii39 <usimarutetunori3@outlook.jp>
2025-02-25 12:13:05 +0900

    高度の最大値を計算する関数を追加し、最大平均部分配列の関数を削除

--- leetCode.py
+++ leetCode.py
@@ -1,21 +1,21 @@
-# https://leetcode.com/problems/maximum-average-subarray-i/?envType=study-plan-v2&envId=leetcode-75
+# https://leetcode.com/problems/find-the-highest-altitude/description/?envType=study-plan-v2&envId=leetcode-75
 from typing import List
 
 
 class Solution:
-    def findMaxAverage(self, nums: List[int], k: int) -> float:
-        # 最初のウィンドウの平均値を初期値とする
-        max_number = sum(nums[:k]) / k
-
-        # 残りのウィンドウをスライドして計算
-        for i in range(len(nums) - k):
-            curr_avg = sum(nums[i + 1 : i + k + 1]) / k
-            max_number = max(max_number, curr_avg)
-
-        return max_number
+    def largestAltitude(self, gain: List[int]) -> int:
+        start = 0
+        maxAltitude = 0
+        if len(gain) == 0:
+            return 0
+        for i in gain:
+            start += i
+            print(start)
+            print(maxAltitude)
+            if start > maxAltitude:
+                maxAltitude = start
+        return maxAltitude
 
 
 s = Solution()
-print(s.findMaxAverage(nums=[5], k=1))  # 5
-print(s.findMaxAverage(nums=[-1], k=1))  # -1
-print(s.findMaxAverage(nums=[1, 12, -5, -6, 50, 3], k=4))  # 12.75
+s.largestAltitude(gain=[-5, 1, 5, 0, -7])
